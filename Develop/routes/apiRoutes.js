687fb71ce8d8193ba7e2b8832c6695bb209fe1ca
Maynard Peralta <maynperalta@gmail.com>
2020-10-22 21:40:36 -0600

    created apiRoutes.js file

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const Workout = require("../public/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id, { $push: { exercises: body}},
+        { new: true, runValidators: true })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router;
