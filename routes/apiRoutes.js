265ae59f69a31e713fb774406614c9d35c6f92a0
Maynard Peralta <maynperalta@gmail.com>
2020-10-24 14:44:47 -0600

    Fixed apiRoutes to get stats

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -31,7 +31,7 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", ({ query }, res) => {
-    Workout.find({ day: { $gte: query.start, $lte: query.end}})
+    Workout.find({}).limit(7)
         .then(workouts => {
             res.json(workouts);
         }).catch(err => {
