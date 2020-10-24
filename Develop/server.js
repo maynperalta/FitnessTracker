b069de60aed4fa8d3763cc7d3970f8c4dd935323
Maynard Peralta <maynperalta@gmail.com>
2020-10-24 00:48:23 -0600

    fixed Morgan middleware code

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
